64048482d50308bdd2b851dc48f412c100a17925
Mykyta N <nesspire00@gmail.com>
2017-04-13 16:41:29 -0400

    Initial commit

---
+++ db.php
@@ -0,0 +1,7 @@
+<?php
+
+//connect to db
+$conn = new PDO('mysql:host=sql.computerstudi.es;dbname=gc200348171', 'gc200348171', 'YaTa2qzm');
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+?>
